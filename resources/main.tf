1d6f922e21931ec0b2ef19d95e30d9f64c76e05f
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-06-28 20:40:24 +0400

    Updated aws vpc

--- resources/main.tf
+++ resources/main.tf
@@ -12,24 +12,25 @@ resource "aws_security_group" "allow_ports_firewall_roboshop" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ports_firewall_roboshop" {
   security_group_id = aws_security_group.allow_ports_firewall_roboshop.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = aws_vpc_security_group_egress_rule.allow_all_traffic_ipv4.cidr_ipv4
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_ports_firewall_roboshop.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_ports_firewall_roboshop.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_ports_firewall_roboshop.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
 
 resource "aws_instance" "frontend" {
   ami           = "ami-00adafae70b8029d8"
