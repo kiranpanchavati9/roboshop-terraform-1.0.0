af0b2dcfbd89ca7d750df7c22984b47a4a59b0d8
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-01 18:42:20 +0400

    Done the final configs

--- resources/main.tf
+++ resources/main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ports_firewall_roboshop" {
   ip_protocol       = "tcp"
   to_port           = 443
 }
-
+ 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_ports_firewall_roboshop.id
@@ -31,6 +31,10 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+
+## FRONTEND DEV EC2 INSTANCE
+
 
 resource "aws_instance" "frontend-dev" {
   ami           = "ami-00adafae70b8029d8"
@@ -49,3 +53,192 @@ resource "aws_route53_record" "frontend-dev" {
   ttl     = 300
   records = [aws_instance.frontend-dev.public_ip]
 }
+
+## MONGODB DEV EC2 INSTANCE 
+
+resource "aws_instance" "mongodb-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "mongodb-dev"
+  }
+}
+
+resource "aws_route53_record" "mongodb-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "mongodb-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb-dev.public_ip]
+}
+
+## CATALOGUE DEV EC2 INSTANCE
+
+resource "aws_instance" "catalogue-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "catalogue-dev"
+  }
+}
+
+resource "aws_route53_record" "catalogue-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "catalogue-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue-dev.public_ip]
+}
+
+
+##  REDIS DEV EC2 INSTANCE
+
+resource "aws_instance" "redis-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "redis-dev"
+  }
+}
+
+resource "aws_route53_record" "redis-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "redis-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis-dev.public_ip]
+}
+
+## USER DEV EC2 INSTANCE
+
+
+resource "aws_instance" "user-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "user-dev"
+  }
+}
+
+resource "aws_route53_record" "user-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "user-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user-dev.public_ip]
+}
+
+
+## CART DEV EC2 INSTANCE
+
+resource "aws_instance" "cart-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "cart-dev"
+  }
+}
+
+resource "aws_route53_record" "cart-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "cart-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart-dev.public_ip]
+}
+
+
+## MYSQL DEV EC2 INSTANCE
+
+resource "aws_instance" "mysql-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "mysql-dev"
+  }
+}
+
+resource "aws_route53_record" "mysql-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql-dev.public_ip]
+}
+
+## SHIPPING DEV EC2 INSTANCE
+
+
+resource "aws_instance" "shipping-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "shipping-dev"
+  }
+}
+
+resource "aws_route53_record" "shipping-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "shipping-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.shipping-dev.public_ip]
+}
+
+## RABBITMQ DEV EC2 INSTANCE
+
+resource "aws_instance" "rabbitmq-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "rabbitmq-dev"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "rabbitmq-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq-dev.public_ip]
+}
+
+
+## PAYMENT DEV EC2 INSTANCE
+
+resource "aws_instance" "payment-dev" {
+  ami           = "ami-00adafae70b8029d8"
+  instance_type = "t3.small"
+  security_groups = [aws_security_group.allow_ports_firewall_roboshop.name]
+  key_name     = "aws-helpag"
+  tags = {
+    Name = "payment-dev"
+  }
+}
+
+resource "aws_route53_record" "payment-dev" {
+  zone_id = "Z01214421PKKTLXAI5VN5"
+  name    = "payment-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.payment-dev.public_ip]
+}
+
+
+## End of Terraform configuration for AWS resources
